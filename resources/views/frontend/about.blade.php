fad303b20c7eb1a52d88ebe53ed375ac128410e7
MD SUJAN AHMED <csesujon155@gmail.com>
2024-01-25 04:49:09 +0600

    front about page mastering

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -4,10 +4,19 @@
 
     {{-- SECTION START --}}
     @include('frontend.layouts.header')
-    @include('frontend.layouts.banner')
-    @include('frontend.layouts.about')
-    @include('frontend.layouts.choose')
-    @include('frontend.layouts.testimonial')
+
+    <!--about section start -->
+        <div class="about_section layout_padding">
+            <div class="container">
+            <div class="image_2"><img src="{{ asset('frontend') }}/images/img-2.png"></div>
+            <h1 class="about_taital">About Our Shop</h1>
+            <div class="image_3"><img src="{{ asset('frontend') }}/images/img-3.png"></div>
+            <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem</p>
+            <div class="read_bt_1"><a href="#">Read More</a></div>
+            </div>
+        </div>
+    <!--about section end -->
+    
     @include('frontend.layouts.contact')
     @include('frontend.layouts.footer')
     {{-- END SECTION --}}
